72ec2ee74b9717c1584049e7c6cd44f84096e52a
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-10-21 12:52:44 +0600

    delete button is working fine

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,8 +1,24 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-
-const Cart = ({cart}) => {
+const Cart = ({cart, carts, setCarts}) => {
 
     const { _id, productImg, productName, brand_name, productType, price, description, rating } = cart;
+
+    const handleDelete = (_id)=>{
+
+        fetch(`http://localhost:5000/carts/${_id}`,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data =>{
+            if(data.deletedCount > 0){
+                toast.success('Deleted from cart successfully');
+                const remaining = carts.filter(el => el._id !== _id);
+                setCarts(remaining);
+            }
+        })
+    }
 
     return (
         <div className="card bg-base-100 shadow-xl">
@@ -16,7 +32,7 @@ const Cart = ({cart}) => {
                 <p>Rating: {rating} / 5</p>
                 <div className="card-actions">
                    
-                   <button onClick={()=> handleDelete(_id)} className="btn bg-[#ff81c0] w-full">Delete</button>
+                   <button onClick={()=> handleDelete(_id)} className="btn bg-red-500 w-full">Delete</button>
                    
                    
                 </div>
--- src/Components/MyCart.jsx
+++ src/Components/MyCart.jsx
@@ -1,4 +1,3 @@
-import { useLoaderData } from "react-router-dom";
 import Cart from "./Cart";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthProvider";
@@ -8,7 +7,7 @@ const MyCart = () => {
 
     const {user} = useContext(AuthContext);
     const [carts, setCarts] = useState([]);
-    const email = user.email;
+    const email = user?.email;
 
    useEffect(()=>{
     fetch(`http://localhost:5000/carts/${email}`)
@@ -16,11 +15,11 @@ const MyCart = () => {
     .then(data => setCarts(data))
    },[email])
 
-   console.log(carts);
+   
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {
-                carts?.map(cart => <Cart key={cart._id} cart={cart}></Cart>)
+                carts?.map(cart => <Cart carts={carts} setCarts={setCarts} key={cart._id} cart={cart}></Cart>)
             }
         </div>
     );
