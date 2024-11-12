013dd4153b01f84df5a44bb25800293ded10c351
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-11-12 19:01:53 +0600

    router is made separately

---
+++ src/Components/Router/Router.jsx
@@ -0,0 +1,85 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from './../Root/Root';
+import ErrorPage from './../ErrorPage/ErrorPage';
+import Home from './../Home/Home';
+import AddProducts from './../AddProducts/AddProducts';
+import MyCart from './../MyCart/MyCart';
+import Login from './../Login/Login';
+import Register from './../Register/Register';
+import ProductDetails from './../ProductDetails/ProductDetails';
+import UpdateProducts from './../UpdateProduct/UpdateProduct';
+import BrandDetails from './../BrandDetails/BrandDetails';
+import PrivateRoute from './../PrivateRoute/PrivateRoute';
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>
+        },
+        {
+          path: "/addProduct",
+          element: (
+            <PrivateRoute>
+              <AddProducts></AddProducts>
+            </PrivateRoute>
+          ),
+        },
+        {
+          path: "/myCart",
+          element: (
+            <PrivateRoute>
+              <MyCart></MyCart>
+            </PrivateRoute>
+          ),
+        },
+        {
+          path: "/login",
+          element: <Login></Login>,
+        },
+        {
+          path: "/register",
+          element: <Register></Register>,
+        },
+        {
+          path: "/productDetails/:id",
+          element: (
+            <PrivateRoute>
+              <ProductDetails></ProductDetails>
+            </PrivateRoute>
+          ),
+          loader: ({ params }) =>
+            fetch(
+              `https://aesthetica-server-site-9lvrk8db1-md-rafiul-islams-projects.vercel.app/product/${params.id}`
+            ),
+        },
+        {
+          path: "/update/:id",
+          element: (
+            <PrivateRoute>
+              <UpdateProducts></UpdateProducts>
+            </PrivateRoute>
+          ),
+          loader: ({ params }) =>
+            fetch(
+              `https://aesthetica-server-site-9lvrk8db1-md-rafiul-islams-projects.vercel.app/product/${params.id}`
+            ),
+        },
+        {
+          path: "/details/:brand_name",
+          element: <BrandDetails></BrandDetails>,
+          loader: ({ params }) =>
+            fetch(
+              `https://aesthetica-server-site-9lvrk8db1-md-rafiul-islams-projects.vercel.app/products/${params.brand_name}`
+            ),
+        },
+      ],
+    },
+  ]);
+
+export default router;  
--- src/main.jsx
+++ src/main.jsx
@@ -1,96 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./Components/Root/Root.jsx";
-import Home from "./Components/Home/Home.jsx";
-import AddProducts from "./Components/AddProducts/AddProducts.jsx";
-import MyCart from "./Components/MyCart/MyCart.jsx";
-import Login from "./Components/Login/Login.jsx";
-import Register from "./Components/Register/Register.jsx";
-import BrandDetails from "./Components/BrandDetails/BrandDetails.jsx";
-import ErrorPage from "./Components/ErrorPage/ErrorPage.jsx";
-import ProductDetails from "./Components/ProductDetails/ProductDetails.jsx";
-import UpdateProduct from "./Components/UpdateProduct/UpdateProduct.jsx";
+import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./Components/AuthProvider/AuthProvider.jsx";
-import PrivateRoute from "./Components/PrivateRoute/PrivateRoute.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root></Root>,
-    errorElement: <ErrorPage></ErrorPage>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>
-      },
-      {
-        path: "/addProduct",
-        element: (
-          <PrivateRoute>
-            <AddProducts></AddProducts>
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: "/myCart",
-        element: (
-          <PrivateRoute>
-            <MyCart></MyCart>
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: "/login",
-        element: <Login></Login>,
-      },
-      {
-        path: "/register",
-        element: <Register></Register>,
-      },
-      {
-        path: "/productDetails/:id",
-        element: (
-          <PrivateRoute>
-            <ProductDetails></ProductDetails>
-          </PrivateRoute>
-        ),
-        loader: ({ params }) =>
-          fetch(
-            `https://aesthetica-server-site-9lvrk8db1-md-rafiul-islams-projects.vercel.app/product/${params.id}`
-          ),
-      },
-      {
-        path: "/update/:id",
-        element: (
-          <PrivateRoute>
-            <UpdateProduct></UpdateProduct>
-          </PrivateRoute>
-        ),
-        loader: ({ params }) =>
-          fetch(
-            `https://aesthetica-server-site-9lvrk8db1-md-rafiul-islams-projects.vercel.app/product/${params.id}`
-          ),
-      },
-      {
-        path: "/details/:brand_name",
-        element: <BrandDetails></BrandDetails>,
-        loader: ({ params }) =>
-          fetch(
-            `https://aesthetica-server-site-9lvrk8db1-md-rafiul-islams-projects.vercel.app/products/${params.brand_name}`
-          ),
-      },
-    ],
-  },
-]);
-
-import {
-  QueryClient,
-  QueryClientProvider
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import router from "./Components/Router/Router.jsx";
 
 const queryClient = new QueryClient()
 
