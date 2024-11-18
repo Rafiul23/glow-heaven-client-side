cc725dbc27b1b7e1763bf9e895c9cd86bfcfff4d
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-11-18 19:16:04 +0600

    make user admin is successful

--- src/Components/ManageUsers/ManageUsers.jsx
+++ src/Components/ManageUsers/ManageUsers.jsx
@@ -1,61 +1,81 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import SectionTitle from "../SectionTitle/SectionTitle";
-import { FaTrashCan } from "react-icons/fa6";
+import { FaTrashCan, FaUser } from "react-icons/fa6";
 import Swal from "sweetalert2";
 
 const ManageUsers = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
 
-    const axiosSecure = useAxiosSecure();
-    const {data: users=[], refetch} = useQuery({
-        queryKey: ['users'],
-        queryFn: async()=>{
-            const res = await axiosSecure.get('/users');
-            return res.data;
-        }
-    })
-
-    const handleDeleteUser = (_id)=>{
-        Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-          if (result.isConfirmed) {
-              axiosSecure.delete(`/carts/${_id}`)
-              .then(res =>{
-                  if(res.data.deletedCount > 0){
-                      refetch();
-                      Swal.fire({
-                          title: "Deleted!",
-                          text: "User has been deleted.",
-                          icon: "success"
-                        });
-                  }
-              })
-            
+  const handleDeleteUser = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${_id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "User has been deleted.",
+              icon: "success",
+            });
           }
         });
       }
-    return (
-        <div>
-            <SectionTitle
-            subHeading={'Who are there?'}
-            heading={'Check all your users'}
-            ></SectionTitle>
+    });
+  };
 
-<div className="overflow-x-auto">
+  const handleMakeAdmin = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Confirm!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.patch(`/users/${_id}`).then((res) => {
+          if (res.data.modifiedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Success!",
+              text: "User has been an admin.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+  return (
+    <div>
+      <SectionTitle
+        subHeading={"Who are there?"}
+        heading={"Check all your users"}
+      ></SectionTitle>
+
+      <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
           <thead className="bg-[#ffdbac] text-[#800]">
             <tr>
-              <th>
-                SL No.
-              </th>
+              <th>SL No.</th>
               <th>Image</th>
               <th>Name</th>
               <th>Email</th>
@@ -74,14 +94,28 @@ const ManageUsers = () => {
                         <img src={user.photoURL} alt="user" />
                       </div>
                     </div>
-                   
                   </div>
                 </td>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td> {user.role}</td>
                 <td>
-                  <button onClick={()=> handleDeleteUser(user._id)} className="btn text-red-500">
+                  {" "}
+                  {user.role === "admin" ? (
+                    user.role
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user?._id)}
+                      className="btn bg-green-600 text-white"
+                    >
+                      <FaUser></FaUser>{" "}
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user._id)}
+                    className="btn text-red-500"
+                  >
                     <FaTrashCan></FaTrashCan>
                   </button>
                 </td>
@@ -90,8 +124,8 @@ const ManageUsers = () => {
           </tbody>
         </table>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default ManageUsers;
