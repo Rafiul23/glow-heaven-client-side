9fcfe90b83c7bae7dff5c3d5c01d440b543e5dbc
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-11-30 21:48:00 +0600

    modified axiosSecure

--- src/Components/UserHome/UserHome.jsx
+++ src/Components/UserHome/UserHome.jsx
@@ -5,7 +5,7 @@ import { HiCurrencyDollar } from "react-icons/hi";
 import { FaCarSide } from "react-icons/fa6";
 
 const UserHome = () => {
-    const { user } = useAuth();
+  const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
   const { data: stats = [] } = useQuery({
@@ -15,26 +15,26 @@ const UserHome = () => {
       return res.data;
     },
   });
-    return (
-        <div className="py-4">
-           <h1 className="text-4xl font-bold">Hi {user?.displayName}! Welcome Back!</h1> 
-        <div className="grid md:grid-cols-2 grid-cols-1 gap-4 my-5">
-            <div className="h-[200px] w-full flex flex-col justify-center items-center text-white bg-purple-500">
-            <HiCurrencyDollar className="text-4xl" />
-            <h3 className="text-2xl font-bold">Payments</h3>
-            <p className="text-3xl">{stats?.revenue}</p>
-            </div>
-            
-            <div className="h-[200px] w-full flex flex-col justify-center items-center text-white bg-pink-500">
-            <FaCarSide className="text-4xl" />
-            <h3 className="text-2xl font-bold">Orders</h3>
-            <p className="text-3xl">{stats?.orders}</p>
-            </div>
-           
+  return (
+    <div className="py-4">
+      <h1 className="text-4xl font-bold">
+        Hi {user?.displayName}! Welcome Back!
+      </h1>
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-4 my-5">
+        <div className="h-[200px] w-full flex flex-col justify-center items-center text-white bg-purple-500">
+          <HiCurrencyDollar className="text-4xl" />
+          <h3 className="text-2xl font-bold">Payments</h3>
+          <p className="text-3xl">{stats?.revenue}</p>
         </div>
 
+        <div className="h-[200px] w-full flex flex-col justify-center items-center text-white bg-pink-500">
+          <FaCarSide className="text-4xl" />
+          <h3 className="text-2xl font-bold">Orders</h3>
+          <p className="text-3xl">{stats?.orders}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserHome;
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,32 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
-  // withCredentials: true,
+  withCredentials: true,
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+
+  axiosSecure.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (err) => {
+      console.log(err);
+      const status = err?.response?.status;
+
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(err);
+    }
+  );
+
   return axiosSecure;
 };
 
