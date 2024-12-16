b2b462f12131f02fc0bb8c5af0d4fdf219b08781
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-12-16 19:13:51 +0600

    added responsiveness at speciality component

--- src/Components/Speciality/Speciality.jsx
+++ src/Components/Speciality/Speciality.jsx
@@ -14,22 +14,32 @@ const Speciality = () => {
         >
             <div className="my-10 p-4">
                 <SectionTitle
-                heading={'Our speciality'}
-                subHeading={'Check our speciality'}
+                    heading={'Our Speciality'}
+                    subHeading={'Check our speciality'}
                 ></SectionTitle>
 
                 <div className="grid bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 grid-cols-1 md:grid-cols-3 gap-4">
-                    <div className="flex justify-center items-center w-full h-[200px] rounded-xl">
-                        <h2 className="text-white text-3xl font-bold">100% Authentic Products</h2>
+                   
+                    <div className="flex justify-center items-center w-full h-[200px] md:h-[250px] rounded-xl">
+                        <h2 className="text-white text-xl md:text-2xl lg:text-3xl font-bold text-center">
+                            100% Authentic Products
+                        </h2>
                     </div>
-                    <div className="flex justify-center items-center w-full md:h-[200px] h-[100px] rounded-xl">
-                        <h2 className="text-white text-3xl font-bold">500+ Beauty Products</h2>
+
+                   
+                    <div className="flex justify-center items-center w-full h-[200px] md:h-[250px] rounded-xl">
+                        <h2 className="text-white text-xl md:text-2xl lg:text-3xl font-bold text-center">
+                            500+ Beauty Products
+                        </h2>
                     </div>
-                    <div className="flex justify-center items-center w-full h-[200px] rounded-xl">
-                        <h2 className="text-white text-3xl font-bold">Free Beauty Consultency</h2>
+
+                    
+                    <div className="flex justify-center items-center w-full h-[200px] md:h-[250px] rounded-xl">
+                        <h2 className="text-white text-xl md:text-2xl lg:text-3xl font-bold text-center">
+                            Free Beauty Consultancy
+                        </h2>
                     </div>
                 </div>
-
             </div>
         </motion.div>
     );
